3b33298fc3f9e3cb858b8a9bb6281be03f1f5586
David Valek <david.valek17@gmail.com>
2026-02-27 12:49:44 +0100

    Update doc comment

--- app/model/Task/FinishType.ts
+++ app/model/Task/FinishType.ts
@@ -1,5 +1,9 @@
 /**
  * Defines the type of task completion.
+ * It is basically what a user needs to do in the system, eg adding user, or anwering questions,
+ * But the main point of this is that those asks are not able to be completed until the user has 
+ * finished the activity -> eg. user could not answer how many children have one allergen until
+ * they fixed SQL query that is responsible for fetching that data.
  */
 export enum FinishType {
     /**
@@ -15,5 +19,10 @@ export enum FinishType {
     /**
      * Task is completed after the user has selected options related to the activity.
      */
-    SELECT_OPTIONS = 'select-options'
+    SELECT_OPTIONS = 'select-options',
+
+    /**
+     * Represents an activity where the user needs to type the correct answer.
+     */
+    TYPE_CORRECT = 'type-correct'
 }
