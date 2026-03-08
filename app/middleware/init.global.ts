3ab1ca194a0d2a2bc219b20dd36006bbee7250fe
David Valek <david.valek17@gmail.com>
2026-03-08 19:41:57 +0100

    Create middleware for loading data from indexed db

---
+++ app/middleware/init.global.ts
@@ -0,0 +1,22 @@
+export default defineNuxtRouteMiddleware(async (to, from) => {
+
+    // 1) Load systems from IndexedDB
+    const result = await IndexedDbStorage.GetStoredInformationSystems()
+    console.log(result.toString())
+    const systemsStore = useSystemsStore()
+    systemsStore.systems = result.data || []
+
+    // 2) Check if system is selected
+    if (to.params.id) {
+        systemsStore.selectedSystemId = to.params.id as string
+    }
+
+    // 3) Check if system is initialized
+    const system = systemsStore.getSystemById(systemsStore.selectedSystemId!)
+    if (system && system.database) {
+        if (!await system.database.isDatabaseInitialized()) {
+            console.log("Initializing DB for system " + systemsStore.selectedSystemId)
+            await system.database.initializeDatabase()
+        }
+    }
+})
